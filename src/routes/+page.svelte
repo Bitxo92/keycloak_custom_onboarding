a9b57180c7967f97d64c08abe1901af5cd75cdd9
Alejandro Patiño García <alexpatino92@yahoo.com>
2025-10-15 15:31:25 +0200

    style(login): modified UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -83,11 +83,6 @@
 			isLoading = false;
 		}
 	};
-
-	// Fallback: Use Keycloak's login page
-	const handleKeycloakLogin = () => {
-		keycloak.login();
-	};
 </script>
 
 <div class="flex h-screen w-screen items-center justify-center">
@@ -133,21 +128,8 @@
 			</form>
 		</Card.Content>
 		<Card.Footer class="flex-col gap-2">
-			<Button onclick={handleCustomLogin} class="w-full" disabled={isLoading}>
+			<Button onclick={handleCustomLogin} class="hover: w-full cursor-pointer" disabled={isLoading}>
 				{isLoading ? 'Logging in...' : 'Login'}
-			</Button>
-
-			<div class="relative my-2">
-				<div class="absolute inset-0 flex items-center">
-					<span class="w-full border-t" />
-				</div>
-				<div class="relative flex justify-center text-xs uppercase">
-					<span class="bg-background px-2 text-muted-foreground">Or</span>
-				</div>
-			</div>
-
-			<Button variant="outline" class="w-full" onclick={handleKeycloakLogin} disabled={isLoading}>
-				Use Keycloak Login Page
 			</Button>
 		</Card.Footer>
 	</Card.Root>
