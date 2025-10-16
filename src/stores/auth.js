0a7ddaa3782002681c9dcf25779fdad5427e1d98
Alejandro Patiño García <alexpatino92@yahoo.com>
2025-10-16 08:07:39 +0200

    docs(authy store): commented code

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -2,25 +2,28 @@ import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
 import keycloak, { initKeycloak } from '$lib/keycloak.js';
 
+// stores for holding app state
 export const isAuthenticated = writable(false);
 export const user = writable(null);
 export const token = writable(null);
 
+//Ensures the code only runs in browser, since our target auth service is browser focused
 export const initializeAuth = async () => {
 	if (!browser) return;
 
 	try {
+		// Initialize keycloak and sets our auth state
 		const authenticated = await initKeycloak();
 		isAuthenticated.set(authenticated);
 
 		if (authenticated) {
 			// @ts-ignore
-			user.set(keycloak.tokenParsed);
+			user.set(keycloak.tokenParsed); // decoded token payload
 			// @ts-ignore
-			token.set(keycloak.token);
+			token.set(keycloak.token); //raw keycloak-jwt token
 		}
 
-		// Update token before it expires
+		// Token auto-refresh every 30s
 		keycloak.onTokenExpired = () => {
 			keycloak.updateToken(30).then((refreshed) => {
 				if (refreshed) {
@@ -36,6 +39,8 @@ export const initializeAuth = async () => {
 		isAuthenticated.set(false);
 	}
 };
+
+// Helper Functions -> triggers keycloaks built in onboarding functions
 
 export const login = () => {
 	if (browser) {
